9295aea19be8fce2b2e2bcd8cef761fe70c486ff
Digvijay Patel <dpat19122@gmail.com>
2025-08-16 15:32:53 +0530

    login page added

--- app.py
+++ app.py
@@ -1,23 +1,37 @@
-from flask import Flask, render_template
-from db import load_data_from_db
-from db import load_job_info
+from flask import Flask, render_template, request, url_for, redirect
+from db import load_data_from_db, load_user_info, load_job_info
 from flask import jsonify
 
 app = Flask(__name__)
 
 
-
+#route home page
 @app.route("/")
 def home():
     jobs = load_data_from_db()
     return render_template("home.html", jobs = jobs)
 
+#route the job profile
 @app.route("/job/<job_id>")
 def profile(job_id):
     profile = load_job_info(job_id)
     if not profile:
         return "Not Found", 404
     return render_template("profile.html", job=profile)
+
+#route the login page
+@app.route("/login", methods=["GET", "POST"])
+def login_page():
+    if request.method == "POST":
+        email = request.form["email"]
+        password = request.form["password"]
+
+        # Save into DB
+        load_user_info({"email": email, "password": password})
+
+        return redirect(url_for("home"))
+
+    return render_template("login_form.html")
 
 
 
--- db.py
+++ db.py
@@ -14,6 +14,7 @@ SSL_CA_PATH = os.getenv("SSL_CA_PATH")
 
 DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
+#connecting with the database
 engine = create_engine(
     DATABASE_URL,
     connect_args={
@@ -23,6 +24,7 @@ engine = create_engine(
     }
 )
 
+#getting the jobs data form the database
 def load_data_from_db():
     with engine.connect() as conn:
         result = conn.execute(text("SELECT * FROM jobs"))
@@ -32,6 +34,7 @@ def load_data_from_db():
             jobs.append(dict(row._mapping))
         return jobs
     
+#getting the spaecific data form the database whose id is defined
 def load_job_info(job_id):
     with engine.connect() as conn:
         result = conn.execute(text("SELECT * FROM jobs WHERE id = :val"), {"val" : job_id})
@@ -39,3 +42,12 @@ def load_job_info(job_id):
         row = result.mappings().fetchone()
 
         return dict(row) if row else None
+    
+
+# load the user data to the database
+def load_user_info(data):
+    print("DEBUG: inserting ->", data)
+    with engine.begin() as conn:  # auto-commit
+        query = text("INSERT INTO user (email, password) VALUES (:email, :password)")
+        result = conn.execute(query, {"email": data['email'], "password": data['password']})
+        print("DEBUG: rows inserted ->", result.rowcount)
